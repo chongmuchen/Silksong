6b525b4b85905d2aea41e35be58cc99563bf8c06
chongmuchen <chongmu.chen@gmail.com>
2026-08-05 23:21:02 +0800

    bootstrap

---
+++ Assets/Scripts/UI/LanguageSelectionButton.cs
@@ -0,0 +1,43 @@
+using UnityEngine.EventSystems;
+
+namespace UnityEngine.UI
+{
+	// todo: 临时
+    // public class LanguageSelectionButton : MenuButton, ISubmitHandler, IEventSystemHandler, IPointerClickHandler
+    // {
+    //     [Header("Language")]
+    //     [SerializeField]
+    //     private string language;
+    //
+    //     [SerializeField]
+    //     private LanguageSelector languageSelector;
+    //
+    //     public string Language => language;
+    //
+    //     protected override void Awake()
+    //     {
+    //         base.Awake();
+    //         if (!languageSelector)
+    //         {
+    //             languageSelector = GetComponentInParent<LanguageSelector>();
+    //         }
+    //     }
+    //
+    //     public new void OnSubmit(BaseEventData eventData)
+    //     {
+    //         if (base.interactable)
+    //         {
+    //             base.OnSubmit(eventData);
+    //             if ((bool)languageSelector)
+    //             {
+    //                 languageSelector.SetLanguage(this);
+    //             }
+    //         }
+    //     }
+    //
+    //     public new void OnPointerClick(PointerEventData eventData)
+    //     {
+    //         OnSubmit(eventData);
+    //     }
+    // }
+}
